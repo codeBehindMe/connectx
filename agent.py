477937c815440b5c4d900c36c1940a35388e72e3
codeBehindMe <aaron.tillekeratne@gmail.com>
2022-09-30 21:46:31 +0000

    Added policy update

--- agent.py
+++ agent.py
@@ -31,28 +31,39 @@ def tuplise(m):
 
 
 class Policy:
-    def __init__(self, num_columns: int, epsilon: float) -> None:
+    def __init__(
+        self, num_columns: int, epsilon: float, alpha: float = 0.01, gamma=0.9
+    ) -> None:
 
         self.num_columns = num_columns
         self.q = defaultdict(lambda: np.zeros(num_columns))
         self.epsilon = epsilon
-
-        self.current_board: tuple = None
+        self.alpha = alpha
+        self.gamma = gamma
 
     def _make_callback(self, board: list, action: int) -> callable:
+        board = tuple(board)
+
         def _callback(reward, next_board):
-            self.current_board = board
-            a = action
-            r = reward
-            n = next_board
+            next_board = tuple(next_board)
+            max_q_s_prime_a = self.q[next_board][self.get_greedy_action(next_board)]
+            # print(f"pre update q(s,a): {self.q[board][action]}")
+            self.q[board][action] += self.alpha * (
+                reward + self.gamma * (max_q_s_prime_a - self.q[board][action])
+            )
+            # print(f"post update q(s,a): {self.q[board][action]}")
             return
 
         return _callback
 
     @tuplise
     def get_greedy_action(self, board: list) -> int:
+        # We set the values of the unavailable functions to be negative inf
+        unavailable_actions_mask = where_equals(board[0 : self.num_columns], 0, True)
+        self.q[board][unavailable_actions_mask] = np.NINF
+        action = int(argmax(self.q[board]))
 
-        return argmax(self.q[board])
+        return action
 
     @tuplise
     def get_epsilon_greedy_action(self, board: list) -> Tuple[int, callable]:
@@ -65,12 +76,7 @@ class Policy:
             # Pick a random action from available actions.
             action = int(np.random.choice(available_actions_mask))
         else:
-            # We set the values of the unavailable functions to be negative inf
-            unavailable_actions_mask = where_equals(
-                board[0 : self.num_columns], 0, True
-            )
-            self.q[board][unavailable_actions_mask] = np.NINF
-            action = int(argmax(self.q[board]))
+            action = self.get_greedy_action(board)
 
         return action, self._make_callback(board, action)
 
