2716ff1be6f0f9266e5d814eef8a64b9a211eb40
codeBehindMe <aaron.tillekeratne@gmail.com>
2022-09-30 21:46:43 +0000

    Added some logging steps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kaggle_environments import evaluate, make, utils
 from agent import Policy
+import numpy as np
 
 
 if __name__ == "__main__":
@@ -10,15 +11,22 @@ if __name__ == "__main__":
     trainer = env.train([None, "random"])
 
     policy = Policy(env.configuration.columns, 0.1)
+    steps_per_game_sum = 0
+    game_count = 0
+    with open("average_steps.hist", "w") as f:
 
-    for _ in range(1000):
-        observation = trainer.reset()
-        steps = 0
-        while not env.done:
-            action, update_policy_fn = policy.get_epsilon_greedy_action(
-                observation.board
-            )
-            observation, reward, done, info = trainer.step(action)
-            update_policy_fn(reward, observation.board)
-            steps += 1
-        print(f"episode steps: {steps}")
+        for _ in range(1000000):
+            observation = trainer.reset()
+            steps = 0
+            while not env.done:
+                action, update_policy_fn = policy.get_epsilon_greedy_action(
+                    observation.board
+                )
+                observation, reward, done, info = trainer.step(action)
+                update_policy_fn(reward, observation.board)
+                steps += 1
+            print(f"reward : {reward}")
+            steps_per_game_sum += steps
+            game_count += 1
+            print(steps_per_game_sum / game_count)
+            f.write(f"{steps_per_game_sum / game_count},{reward}\n")
