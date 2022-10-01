6fbdc1c0023407ba459bd433696c06012c1a379c
codeBehindMe <aaron.tillekeratne@gmail.com>
2022-10-01 03:38:17 +0000

    Added tensorflow and policy package

---
+++ src/policy/policy.py
@@ -0,0 +1,15 @@
+from abc import ABC
+from abc import abstractmethod
+from typing import Tuple
+
+
+
+class IPolicy(ABC):
+    
+    @abstractmethod
+    def get_epsilon_greedy_action(self, state) -> Tuple[int, callable]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_greedy_action(self, state) -> int:
+        raise NotImplementedError()
